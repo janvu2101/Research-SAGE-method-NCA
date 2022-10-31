e5a88b6ea2aea30bad37770b548d3ea0e330b9e2
janvu2101 <104369461+janvu2101@users.noreply.github.com>
2022-10-31 10:38:14 +0100

    Update NCA.R

--- NCA-SAGE/NCA.R
+++ NCA-SAGE/NCA.R
@@ -7,15 +7,13 @@ dim(df)
 str(df)
 View(df)
 unique(df$ownership)
-df_pri <- df[df$ownership=="Private Equity", ]
-df_gov <- df[df$ownership=="Government", ]
-
-dim(df_pri)
+df <- df[df$ownership=="Private Equity", ]
 
 df_private <- df_private[ , c("ownership","zmanagement","ztargets","zpeople","zallops","zorg_inputs","zorg_demand")]
 View(df_private)
 dim(df_private)
-
+table(is.na(df$zorg_inputs))
+table(is.na(df$zallops))
 #Scatter plot
 plot(df$zallops, df$zmanagement, main="Scatter plot",   xlab="zallops", ylab="zmanagement", col = "red", pch = 19)
 plot(df$zorg_inputs, df$zmanagement, main="Scatter plot",   xlab="zorg_inputs", ylab="zmanagement",col = "blue", pch = 19)
@@ -31,20 +29,10 @@ plot(df$zpeople, df$zmanagement, main="Scatter plot",   xlab="zpeople", ylab="zm
 abline(a =2.4, b = 0.07, col="red")
 
 #NCA
-nca_pri1 <- nca_analysis(df_pri, "zallops", "zpeople", ceilings = c("ce_fdh","cr_fdh"), test.rep = 10000)
-nca_pri2 <- nca_analysis(df_pri, "zorg_inputs", "zpeople", ceilings = c("ce_fdh","cr_fdh"), test.rep = 10000)
-
-
-
-nca_output(nca_pri1, summaries = TRUE)
-nca_output(nca_pri2, summaries = TRUE)
-
-
-
-nca_zallops<-nca_analysis(df,"zallops", "zmanagement", ceilings=c("ce_fdh", "cr_fdh"), test.rep = 10000)
+nca_zallops<-nca_analysis(df,"zallops", "zpeople", ceilings=c("ce_fdh", "cr_fdh"), test.rep = 10000)
 nca_output(nca_zallops, summaries = TRUE)
 nca_output(nca_zallops, bottlenecks = TRUE, summaries = FALSE)
-nca_zinputs<-nca_analysis(df,"zorg_inputs", "zmanagement", ceilings=c("ce_fdh", "cr_fdh"), test.rep = 10000)
+nca_zinputs<-nca_analysis(df,"zorg_inputs", "zpeople", ceilings=c("ce_fdh", "cr_fdh"), test.rep = 10000)
 nca_output(nca_zinputs, summaries = TRUE)
 nca_output(nca_zinputs, bottlenecks = TRUE, summaries = FALSE)
 
@@ -52,5 +40,5 @@ nca_ztargets<-nca_analysis(df,"ztargets","zmanagement",ceilings = c("ce_fdh","cr
 nca_output(nca_ztargets, summaries = TRUE)
 
 #Outliers:
-nca_outliers(df, "zorg_inputs","zmanagement", plotly = TRUE)
-nca_outliers(df, "zallops","zmanagement", plotly = TRUE)
+nca_outliers(df, "zorg_inputs","zpeople", plotly = TRUE)
+nca_outliers(df, "zallops","zpeople", plotly = TRUE)
